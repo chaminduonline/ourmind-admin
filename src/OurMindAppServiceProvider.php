52436d4af3684880831594b73ce7ca4da4202e01
chamindu <chamindu.developer@gmail.com>
2020-09-27 13:15:02 +0530

    inital commit

---
+++ src/OurMindAppServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: chamindu
+ * Date: 9/27/20
+ * Time: 1:04 PM
+ */
+namespace Ourmind\Autoload\Provider;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class OurMindAppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
